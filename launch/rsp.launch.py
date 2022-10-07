4430bf738c0d1e3270043199837ab07ca446f396
Idris-Ibrahim <93320320+Idris-Ibrahim@users.noreply.github.com>
2022-10-07 14:52:54 +0200

    name change

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('P3_edo_guide'))
+    pkg_path = os.path.join(get_package_share_directory('p3_edo_guide'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
